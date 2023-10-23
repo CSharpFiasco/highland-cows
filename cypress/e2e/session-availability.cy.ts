494a8a30ffe92d20e2e95bee5a16c368b7a0e29d
CSharpFiasco <40156600+CSharpFiasco@users.noreply.github.com>
2023-10-23 18:31:44 -0500

    Click through next page and check those dates as well

--- cypress/e2e/session-availability.cy.ts
+++ cypress/e2e/session-availability.cy.ts
@@ -6,12 +6,37 @@ describe('home', () => {
     });
 
     it('should have session button', () => {
+        cy.task('log', 'Testing website: https://book.usesession.com/i/5DeS0FGAL')
+
         cy.get('.session-item-button button')
             .should('be.visible')
             .click();
+        
+        let month = 'unknown';
 
+        // make sure month is visible and set month
+        cy.get('.items-center.h-12 .tracking-normal').should('be.visible').then((x) => {
+            month = x.text();
+        });
+
+        // check to see if a clickable button is available
         cy.get('.px-5.pb-5 button').each((el) => {
-            expect(el.hasClass('text-gray-300')).to.be.true;
+            expect(el.hasClass('text-gray-300'), `Testing button with text: ${el.text()} for ${month}`).to.be.true;
+        });
+
+        // check next month
+        cy.get('.items-center.h-12 button.justify-end')
+            .should('be.visible')
+            .click();
+
+        // make sure month is visible and set month
+        cy.get('.items-center.h-12 .tracking-normal').should('be.visible').then((x) => {
+            month = x.text();
+        });
+
+        // check to see if a clickable button is available
+        cy.get('.px-5.pb-5 button').each((el) => {
+            expect(el.hasClass('text-gray-300'), `Testing button with text: ${el.text()} for ${month}`).to.be.true;
         });
     });
 });
